21d6148bfd7d67d28e64c569a0db2a91ce814437
romulo santana <romulo.ssant@gmail.com>
2026-07-01 13:47:32 -0300

    ajuste na condição das páginas que serão tratadas

--- src/tratamento_apuracoes.py
+++ src/tratamento_apuracoes.py
@@ -534,7 +534,7 @@ def tratar_planilha_apuracao():
         # Carregamento e inserção de colunas dependentes da aba, como por exemplo nome do restaurante e nome da balança.
         for aba_nome in nomes_abas_disponiveis:
             inicio = time.time()
-            if "CONSOLIDADO" in aba_nome.upper() or "ANALITICO" in aba_nome.upper(): continue
+            if "3352 - " not in aba_nome.upper(): continue
 
             print(f"Tratando aba {aba_nome}.")
             nome_planilha = wb.get_sheet_by_name(aba_nome)
